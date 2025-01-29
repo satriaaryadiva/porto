9fec049266e6d0f8e39154fc31c31771d3836c99
Satria Arya Diva <satriaarya125@gmail.com>
2025-01-29 20:28:53 +0700

    test .1.2

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -50,7 +50,6 @@ const ProjectList: React.FC<ProjectListProps> = ({ projects }) => {
             <Link
               href={project.demoLink}
               target="_blank"
-              name={project.name}
               className="inline-block px-4 py-2 mt-4 text-sm font-semibold text-center text-gray-900 bg-accent rounded-lg hover:bg-accent/90"            >
               View Demo
             </Link>
