5dd1acf204dbe60748bb9710e16eb9bf964fced9
Satria Arya Diva <satriaarya125@gmail.com>
2025-09-09 10:15:39 +0700

    test 4

--- src/components/MotionLink.tsx
+++ src/components/MotionLink.tsx
@@ -1,23 +1,27 @@
-/* eslint-disable react/display-name */
 "use client";
 
-import { motion } from "framer-motion";
 import Link, { LinkProps } from "next/link";
-import React, { forwardRef } from "react";
+import { motion } from "framer-motion";
+import { forwardRef, ReactNode } from "react";
 
-// bikin motion wrapper yang ngerti <a>
+type MotionLinkProps = LinkProps & {
+  children: ReactNode;
+  className?: string;
+  target?: string;
+  "aria-label"?: string;
+};
+
 const MotionLink = motion(
-  forwardRef<HTMLAnchorElement, LinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>>(
-    ({ children, href, ...props }, ref) => (
-      <Link href={href} passHref legacyBehavior>
-        <a ref={ref} {...props}>
-          {children}
-        </a>
+  forwardRef<HTMLAnchorElement, MotionLinkProps>(function MotionLink(
+    { children, className, ...props },
+    ref
+  ) {
+    return (
+      <Link {...props} ref={ref} className={className}>
+        {children}
       </Link>
-    )
-  )
+    );
+  })
 );
-
-MotionLink.displayName = "MotionLink";
 
 export default MotionLink;
