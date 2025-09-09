6b4636b6d1960c1d588d83ca5e7e80f065808acb
Satria Arya Diva <satriaarya125@gmail.com>
2025-09-09 10:18:11 +0700

    test 5

--- src/components/navigation/NavButton.tsx
+++ src/components/navigation/NavButton.tsx
@@ -1,6 +1,7 @@
 import {
   Github,
   Home,
+  Instagram,
   InstagramIcon,
   Linkedin,
   NotebookText,
@@ -8,11 +9,12 @@ import {
   Phone,
   User,
 } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 import ResponsiveComponent from "../ResponsiveComponent";
 import clsx from "clsx";
+import { motion } from "framer-motion";
 import { IBtnList } from "@/app/data";
-import MotionLink from "../MotionLink";
 
 const getIcon = (icon: string) => {
   switch (icon) {
@@ -42,6 +44,8 @@ const item = {
   show: { scale: 1 },
 };
 
+const NavLink = motion<any>(Link);
+
 interface INavBtn extends IBtnList {
   x: number | string;
   y: number | string;
@@ -65,13 +69,13 @@ const NavButton = ({
             className="w-fit absolute font-black cursor-pointer z-50"
             style={{ transform: `translate(${x},${y})` }}
           >
-            <MotionLink
+            <NavLink
               variants={item}
               href={link}
               target={newTab ? "_blank" : "_self"}
               className="text-foreground rounded-full flex items-center justify-center custom-bg"
               aria-label={label}
-             
+              name={label}
             >
               <span className="relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent bg-cyan-300 rounded-full text-black">
                 {getIcon(icon)}
@@ -80,31 +84,31 @@ const NavButton = ({
                   {label}
                 </span>
               </span>
-            </MotionLink>
+            </NavLink>
           </div>
         ) : (
           <div className="cursor-pointer z-50">
-            <MotionLink
+            <NavLink
               href={link}
               variants={item}
               target={newTab ? "_blank" : "_self"}
-              className="text-foreground rounded-full font-extrabold flex items-center justify-center custom-bg"
+              className="text-foreground rounded-full  font-extrabold flex items-center justify-center custom-bg"
               aria-label={label}
-             
+              name={label}
             >
               <span className="relative w-10 h-10 bg-cyan-300 text-black text-xl rounded-full font-black xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent">
                 {getIcon(icon)}
-                <span className="peer bg-transparent absolute w-full h-full" />
+                <span className="peer bg-transparent absolute  w-full h-full" />
                 <span
                   className={clsx(
-                    "absolute peer-hover:block px-2 py-1 bg-background text-foreground text-sm rounded-md border-2 border-white font-extrabold shadow-lg whitespace-nowrap",
+                    "absolute   peer-hover:block px-2 py-1     bg-background text-foreground   text-sm rounded-md border-2 border-white font-extraboldshadow-lg whitespace-nowrap",
                     labelDirection === "left" ? "right-full left-auto" : ""
                   )}
                 >
                   {label}
                 </span>
               </span>
-            </MotionLink>
+            </NavLink>
           </div>
         );
       }}
